3e1d533c2390b50a0e1b21b9c9aa397f598b3fe2
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-04-14 17:30:26 -0400

    Dump trees with no protocols, and refactor generation logic

--- src/generate.js
+++ src/generate.js
@@ -3,7 +3,7 @@ const fs = require('fs-extra');
 const he = require('he');
 require('colors');
 
-// Non ES module version of https://github.com/sindresorhus/log-symbols
+// * Non-ES module version of https://github.com/sindresorhus/log-symbols
 const main = {
 	info: 'ℹ'.blue,
 	success: '✔'.green,
@@ -20,11 +20,9 @@ const fallback = {
 
 const logSymbols = isUnicodeSupported() ? main : fallback;
 
+const KINNAY_WIKI_BASE = 'https://github.com/kinnay/NintendoClients/wiki';
 
-let unknownProtocolNameCounter = 0;
-const kinnayWikiBase = 'https://github.com/kinnay/NintendoClients/wiki';
-
-// Standardize the type names to match Kinnay's wiki
+// * Standardize the type names to match Kinnay's wiki
 const COMMON_TYPE_CONVERSIONS = {
 	'qvector<byte>': 'Buffer',
 	'byte': 'Uint8',
@@ -40,54 +38,65 @@ const COMMON_TYPE_CONVERSIONS = {
 	'datetime': 'DateTime',
 	'qresult': 'Result',
 	'stationurl': 'StationURL',
-	'qBuffer': 'qBuffer', // Just so it's found
+	'qBuffer': 'qBuffer', // * Just so it's found
 	'buffer': 'Buffer',
-	'ResultRange': 'ResultRange', // Just so it's found
+	'ResultRange': 'ResultRange', // * Just so it's found
 	'variant': 'Variant',
-	'any<Data,string>': 'AnyDataHolder' // unsure if this can look different
+	'any<Data,string>': 'AnyDataHolder' // * Unsure if this can look different
 };
 
-// Links to common types in Kinnay's wiki
+// * Links to common types in Kinnay's wiki
 const COMMON_TYPE_LINKS = {
-	'String': `${kinnayWikiBase}/NEX-Common-Types#string`,
-	'Buffer': `${kinnayWikiBase}/NEX-Common-Types#buffer`,
-	'qBuffer': `${kinnayWikiBase}/NEX-Common-Types#qbuffer`,
-	'List': `${kinnayWikiBase}/NEX-Common-Types#list`,
-	'Map': `${kinnayWikiBase}/NEX-Common-Types#map`,
-	'PID': `${kinnayWikiBase}/NEX-Common-Types#pid`,
-	'Result': `${kinnayWikiBase}/NEX-Common-Types#result`,
-	'DateTime': `${kinnayWikiBase}/NEX-Common-Types#datetime`,
-	'StationURL': `${kinnayWikiBase}/NEX-Common-Types#stationurl`,
-	'Variant': `${kinnayWikiBase}/NEX-Common-Types#variant`,
-	'Structure': `${kinnayWikiBase}/NEX-Common-Types#structure`,
-	'Data': `${kinnayWikiBase}/NEX-Common-Types#data`,
-	'AnyDataHolder': `${kinnayWikiBase}/NEX-Common-Types#anydataholder`,
-	'RVConnectionData': `${kinnayWikiBase}/NEX-Common-Types#rvconnectiondata`,
-	'ResultRange': `${kinnayWikiBase}/NEX-Common-Types#resultrange`
+	'String': `${KINNAY_WIKI_BASE}/NEX-Common-Types#string`,
+	'Buffer': `${KINNAY_WIKI_BASE}/NEX-Common-Types#buffer`,
+	'qBuffer': `${KINNAY_WIKI_BASE}/NEX-Common-Types#qbuffer`,
+	'List': `${KINNAY_WIKI_BASE}/NEX-Common-Types#list`,
+	'Map': `${KINNAY_WIKI_BASE}/NEX-Common-Types#map`,
+	'PID': `${KINNAY_WIKI_BASE}/NEX-Common-Types#pid`,
+	'Result': `${KINNAY_WIKI_BASE}/NEX-Common-Types#result`,
+	'DateTime': `${KINNAY_WIKI_BASE}/NEX-Common-Types#datetime`,
+	'StationURL': `${KINNAY_WIKI_BASE}/NEX-Common-Types#stationurl`,
+	'Variant': `${KINNAY_WIKI_BASE}/NEX-Common-Types#variant`,
+	'Structure': `${KINNAY_WIKI_BASE}/NEX-Common-Types#structure`,
+	'Data': `${KINNAY_WIKI_BASE}/NEX-Common-Types#data`,
+	'AnyDataHolder': `${KINNAY_WIKI_BASE}/NEX-Common-Types#anydataholder`,
+	'RVConnectionData': `${KINNAY_WIKI_BASE}/NEX-Common-Types#rvconnectiondata`,
+	'ResultRange': `${KINNAY_WIKI_BASE}/NEX-Common-Types#resultrange`
 };
+
+let unknownDDLCount = 0;
+let unknownProtocolNameCounter = 0;
 
 function generateDocumentation(tree, outputPath) {
-	/*-------------------------------------------
-	| First parse out the larger DDL parse tree |
-	|   Into smaller, more managable, pieces    |
-	-------------------------------------------*/
 
+	// * First parse out the larger DDL parse tree
+	// * into smaller, more managable, pieces
 	const protocolMethods = [];
 	const protocolClasses = [];
 	let protocolName;
 	let protocolID;
 
-	for (const element of tree.rootNamespace.elements) {
-		if (element.body instanceof DDL.DDLClassDeclaration) {
-			const className = element.body.typeDeclaration.declaration.nameSpaceItem.parseTreeItem1.name.value;
+	const protocolDeclaration = tree.rootNamespace.elements.find(element => element.body instanceof DDL.DDLProtocolDeclaration);
+	const classDeclarations = tree.rootNamespace.elements.filter(element => element.body instanceof DDL.DDLClassDeclaration);
+
+	if (!protocolDeclaration) {
+		unknownDDLCount += 1;
+		const jsonOutput = `${outputPath}/ddl-${unknownDDLCount++}.json`;
+		console.log(`[${logSymbols.warning}]`, `Found DDL tree with no protocol declaration. Writing to ${jsonOutput}\n`.yellow.bold);
+		fs.writeFileSync(jsonOutput, JSON.stringify(tree, null, 4));
+	} else {
+
+		// * First go over all the classes and carve them out
+		for (const classDeclaration of classDeclarations) {
+			const className = classDeclaration.body.typeDeclaration.declaration.nameSpaceItem.parseTreeItem1.name.value;
 			const classMembers = [];
-			let classParentClassName = element.body.parentClassName.value;
+			let classParentClassName = classDeclaration.body.parentClassName.value;
 
 			if (classParentClassName === '') {
 				classParentClassName = 'Structure';
 			}
 
-			for (const { body: classMember } of element.body.classMembers.elements) {
+			for (const { body: classMember } of classDeclaration.body.classMembers.elements) {
 				const classMemberName = classMember.nameSpaceItem.parseTreeItem1.name.value;
 				const classMemberType = classMember.declarationUse.name.value;
 
@@ -104,69 +113,65 @@ function generateDocumentation(tree, outputPath) {
 			});
 		}
 
-		if (element.body instanceof DDL.DDLProtocolDeclaration) {
-			// Could also be element.body.declaration.unitName.value ?
-			protocolName = element.body.declaration.nameSpaceItem.parseTreeItem1.name.value;
-			protocolID = 'Unknown ID'; // TODO: Find a way to find this!!
+		// * Start building the protocol definition
+		// ? Could also be element.body.declaration.unitName.value
+		protocolName = protocolDeclaration.body.declaration.nameSpaceItem.parseTreeItem1.name.value;
+		protocolID = 'Unknown ID'; // TODO - Find a way to find this!!
 
-			if (!protocolName) {
-				protocolName = `Unknown Protocol - ${unknownProtocolNameCounter++}`;
-				console.log(`[${logSymbols.warning}]`, `Could not determine real protocol name. Defaulting to ${protocolName}`.yellow.bold);
-			}
-
-			console.log(`[${logSymbols.info}]`, `Found NEX protocol: ${protocolName}`.cyan.bold);
-
-			for (const { body: method} of element.body.methods.elements) {
-				const methodName = method.methodDeclaration.declaration.nameSpaceItem.parseTreeItem1.name.value;
-				const methodRequestParameters = [];
-				const methodResponseParameters = [];
-
-				// these are also stored in method.methodDeclaration.parameters
-				for (const { body: parameter } of method.parameters.elements) {
-					const parameterName = parameter.variable.nameSpaceItem.parseTreeItem1.name.value;
-					// This also seems to be stored in parameter.variable.declarationUse
-					const parameterValue = parameter.declarationUse.name.value;
-					const parameterType = parameter.type;
-					const paramaterDefinition = {
-						name: parameterName,
-						value: parameterValue
-					};
-
-					if (parameterType === 1) {
-						methodRequestParameters.push(paramaterDefinition);
-					} else if (parameterType === 2) {
-						methodResponseParameters.push(paramaterDefinition);
-					} else if (parameter instanceof DDL.DDLReturnValue) {
-						// ReturnValue types always come first
-						methodResponseParameters.unshift(paramaterDefinition);
-					} else {
-						throw new Error(`Unknown paramater type ${parameterType}`);
-					}
-				}
-
-				protocolMethods.push({
-					name: methodName,
-					requestParameters: methodRequestParameters,
-					responseParameters: methodResponseParameters,
-				});
-			}
-
-
-			const markdown = buildMarkDown(protocolName, protocolID, protocolMethods, protocolClasses);
-			fs.ensureDirSync(`${outputPath}`);
-			fs.writeFileSync(`${outputPath}/${protocolName}.md`, markdown);
-
-			console.log(`[${logSymbols.success}]`, `Writing protocol documentation to ${outputPath}/${protocolName}.md\n`.green.bold);
+		if (!protocolName) {
+			protocolName = `Unknown Protocol - ${unknownProtocolNameCounter++}`;
+			console.log(`[${logSymbols.warning}]`, `Could not determine real protocol name. Defaulting to ${protocolName}`.yellow.bold);
 		}
-	}
 
-	/*------------------------------------------------
-	| Now start building a .md file for the protocol |
-	------------------------------------------------*/
+		console.log(`[${logSymbols.info}]`, `Found NEX protocol: ${protocolName}`.cyan.bold);
+
+		for (const { body: method} of protocolDeclaration.body.methods.elements) {
+			const methodName = method.methodDeclaration.declaration.nameSpaceItem.parseTreeItem1.name.value;
+			const methodRequestParameters = [];
+			const methodResponseParameters = [];
+
+			// ? These are also stored in method.methodDeclaration.parameters
+			for (const { body: parameter } of method.parameters.elements) {
+				const parameterName = parameter.variable.nameSpaceItem.parseTreeItem1.name.value;
+				// ? This also seems to be stored in parameter.variable.declarationUse
+				const parameterValue = parameter.declarationUse.name.value;
+				const parameterType = parameter.type;
+				const paramaterDefinition = {
+					name: parameterName,
+					value: parameterValue
+				};
+
+				if (parameterType === 1) {
+					methodRequestParameters.push(paramaterDefinition);
+				} else if (parameterType === 2) {
+					methodResponseParameters.push(paramaterDefinition);
+				} else if (parameter instanceof DDL.DDLReturnValue) {
+					// ! ReturnValue types always come first
+					methodResponseParameters.unshift(paramaterDefinition);
+				} else {
+					throw new Error(`Unknown paramater type ${parameterType}`);
+				}
+			}
+
+			protocolMethods.push({
+				name: methodName,
+				requestParameters: methodRequestParameters,
+				responseParameters: methodResponseParameters,
+			});
+		}
+
+
+		const markdown = buildMarkdown(protocolName, protocolID, protocolMethods, protocolClasses);
+		fs.ensureDirSync(`${outputPath}`);
+		fs.writeFileSync(`${outputPath}/${protocolName}.md`, markdown);
+
+		console.log(`[${logSymbols.success}]`, `Writing protocol documentation to ${outputPath}/${protocolName}.md\n`.green.bold);
+	}
 }
 
-function buildMarkDown(protocolName, protocolID, protocolMethods, protocolClasses) {
-	let mdFileContents = `## [NEX-Protocols](${kinnayWikiBase}/NEX-Protocols) > ${protocolName} (${protocolID})`;
+function buildMarkdown(protocolName, protocolID, protocolMethods, protocolClasses) {
+	// * Now start building a .md file for the protocol
+	let mdFileContents = `## [NEX-Protocols](${KINNAY_WIKI_BASE}/NEX-Protocols) > ${protocolName} (${protocolID})`;
 
 	const methodsTable = buildMethodsTable(protocolMethods);
 	mdFileContents += `\n\n${methodsTable}`;
@@ -192,7 +197,7 @@ function buildMethodsTable(protocolMethods) {
 	for (const protocolMethod of protocolMethods) {
 		const methodID = protocolMethods.indexOf(protocolMethod) + 1;
 		const methodHyperlink = `[${protocolMethod.name}](#${methodID}-${protocolMethod.name.toLowerCase()})`;
-		
+
 		table += `\n| ${methodID} | ${methodHyperlink} |`;
 	}
 
@@ -207,48 +212,7 @@ function buildMethodDocumentation(protocolMethod, methodID, protocolClasses) {
 	if (protocolMethod.requestParameters.length === 0) {
 		requestDocumentation += '\nThis method does not take any parameters';
 	} else {
-		requestDocumentation += '\n| Type | Name | Description |';
-		requestDocumentation += '\n| --- | --- | --- |';
-
-		for (const requestParameter of protocolMethod.requestParameters) {
-			let type = requestParameter.value;
-
-			if (COMMON_TYPE_CONVERSIONS[type]) {
-				type = COMMON_TYPE_CONVERSIONS[type];
-			}
-
-			if (COMMON_TYPE_LINKS[type]) {
-				type = `[${type}](${COMMON_TYPE_LINKS[type]})`;
-			}
-
-			// Lists are defined in multiple ways
-			if (type.startsWith('std_list<') || type.startsWith('qvector<') || type.startsWith('qlist<')) {
-				const listParts = type.split(/[<>]/);
-				let listType = listParts[1];
-				const listTypeInFile = protocolClasses.some(({ name }) => name === listType);
-
-				if (listTypeInFile) {
-					listType = `[${listType}](#${listType.toLowerCase()})`;
-				}
-
-				if (COMMON_TYPE_CONVERSIONS[listType]) {
-					listType = COMMON_TYPE_CONVERSIONS[listType];
-				}
-
-				if (COMMON_TYPE_LINKS[listType]) {
-					listType = `[${listType}](${COMMON_TYPE_LINKS[listType]})`;
-				}
-
-				type = `[List](${kinnayWikiBase + '/NEX-Common-Types#list'})<${listType}>`;
-			}
-
-			const typeInFile = protocolClasses.some(({ name }) => name === type);
-			if (typeInFile) {
-				type = `[${type}](#${type.toLowerCase()})`;
-			}
-
-			requestDocumentation += `\n| ${he.encode(type)} | ${requestParameter.name} |  |`;
-		}
+		requestDocumentation += buildParametersTable(protocolMethod.requestParameters, protocolClasses);
 	}
 
 	methodDocumentation += requestDocumentation;
@@ -258,48 +222,7 @@ function buildMethodDocumentation(protocolMethod, methodID, protocolClasses) {
 	if (protocolMethod.responseParameters.length === 0) {
 		responseDocumentation += '\nThis method does not return anything';
 	} else {
-		responseDocumentation += '\n| Type | Name | Description |';
-		responseDocumentation += '\n| --- | --- | --- |';
-
-		for (const responseParameter of protocolMethod.responseParameters) {
-			let type = responseParameter.value;
-
-			if (COMMON_TYPE_CONVERSIONS[type]) {
-				type = COMMON_TYPE_CONVERSIONS[type];
-			}
-
-			if (COMMON_TYPE_LINKS[type]) {
-				type = `[${type}](${COMMON_TYPE_LINKS[type]})`;
-			}
-
-			// Lists are defined in multiple ways
-			if (type.startsWith('std_list<') || type.startsWith('qvector<') || type.startsWith('qlist<')) {
-				const listParts = type.split(/[<>]/);
-				let listType = listParts[1];
-				const listTypeInFile = protocolClasses.some(({ name }) => name === listType);
-
-				if (listTypeInFile) {
-					listType = `[${listType}](#${listType.toLowerCase()})`;
-				}
-
-				if (COMMON_TYPE_CONVERSIONS[listType]) {
-					listType = COMMON_TYPE_CONVERSIONS[listType];
-				}
-
-				if (COMMON_TYPE_LINKS[listType]) {
-					listType = `[${listType}](${COMMON_TYPE_LINKS[listType]})`;
-				}
-
-				type = `[List](${kinnayWikiBase + '/NEX-Common-Types#list'})<${listType}>`;
-			}
-
-			const typeInFile = protocolClasses.some(({ name }) => name === type);
-			if (typeInFile) {
-				type = `[${type}](#${type.toLowerCase()})`;
-			}
-
-			responseDocumentation += `\n| ${he.encode(type)} | ${responseParameter.name} |  |`;
-		}
+		responseDocumentation += buildParametersTable(protocolMethod.responseParameters, protocolClasses);
 	}
 
 	methodDocumentation += responseDocumentation;
@@ -313,7 +236,7 @@ function buildClassesDocumentation(protocolClasses) {
 	for (const protocolClass of protocolClasses) {
 		const parentClassInFile = protocolClasses.some(({ name }) => name === protocolClass.parentClassName);
 		let parentClassName = protocolClass.parentClassName;
-		
+
 		if (parentClassInFile) {
 			parentClassName = `[${parentClassName}](#${parentClassName.toLowerCase()})`;
 		}
@@ -331,42 +254,7 @@ function buildClassesDocumentation(protocolClasses) {
 		classDocumentation += '\n| --- | --- |';
 
 		for (const member of protocolClass.members) {
-			let memberType = member.type;
-			const memberTypeInFile = protocolClasses.some(({ name }) => name === memberType);
-
-			if (memberTypeInFile) {
-				memberType = `[${memberType}](#${memberType.toLowerCase()})`;
-			}
-
-			if (COMMON_TYPE_CONVERSIONS[memberType]) {
-				memberType = COMMON_TYPE_CONVERSIONS[memberType];
-			}
-
-			if (COMMON_TYPE_LINKS[memberType]) {
-				memberType = `[${memberType}](${COMMON_TYPE_LINKS[memberType]})`;
-			}
-
-			// Lists are defined in multiple ways
-			if (memberType.startsWith('std_list<') || memberType.startsWith('qvector<') || memberType.startsWith('qlist<')) {
-				const listParts = memberType.split(/[<>]/);
-				let listType = listParts[1];
-				const listTypeInFile = protocolClasses.some(({ name }) => name === listType);
-
-				if (listTypeInFile) {
-					listType = `[${listType}](#${listType.toLowerCase()})`;
-				}
-
-				if (COMMON_TYPE_CONVERSIONS[listType]) {
-					listType = COMMON_TYPE_CONVERSIONS[listType];
-				}
-
-				if (COMMON_TYPE_LINKS[listType]) {
-					listType = `[${listType}](${COMMON_TYPE_LINKS[listType]})`;
-				}
-
-				memberType = `[List](${kinnayWikiBase + '/NEX-Common-Types#list'})<${listType}>`;
-			}
-
+			const memberType = typeToMarkdown(member.type, protocolClasses);
 			classDocumentation += `\n| ${member.name} | ${he.encode(memberType)} |`;
 		}
 
@@ -376,18 +264,78 @@ function buildClassesDocumentation(protocolClasses) {
 	return classesDocumentation;
 }
 
+function buildParametersTable(parameters, protocolClasses) {
+	let table = '';
+
+	table += '\n| Type | Name | Description |';
+	table += '\n| --- | --- | --- |';
+
+	for (const parameter of parameters) {
+		const type = typeToMarkdown(parameter.value, protocolClasses);
+		table += `\n| ${he.encode(type)} | ${parameter.name} |  |`;
+	}
+
+	return table;
+}
+
+function typeToMarkdown(type, protocolClasses) {
+	if (COMMON_TYPE_CONVERSIONS[type]) {
+		type = COMMON_TYPE_CONVERSIONS[type];
+	}
+
+	if (COMMON_TYPE_LINKS[type]) {
+		type = `[${type}](${COMMON_TYPE_LINKS[type]})`;
+	}
+
+	if (isListType(type)) {
+		type = listTypeToMarkdown(type, protocolClasses);
+	}
+
+	const isProtocolClass = protocolClasses.some(({ name }) => name === type);
+	if (isProtocolClass) {
+		type = `[${type}](#${type.toLowerCase()})`;
+	}
+
+	return type;
+}
+
+function isListType(type) {
+	// * Lists are defined in multiple ways
+	return type.startsWith('std_list<') || type.startsWith('qvector<') || type.startsWith('qlist<');
+}
+
+function listTypeToMarkdown(type, protocolClasses) {
+	const listParts = type.split(/[<>]/);
+	let listType = listParts[1];
+	const listTypeInFile = protocolClasses.some(({ name }) => name === listType);
+
+	if (listTypeInFile) {
+		listType = `[${listType}](#${listType.toLowerCase()})`;
+	}
+
+	if (COMMON_TYPE_CONVERSIONS[listType]) {
+		listType = COMMON_TYPE_CONVERSIONS[listType];
+	}
+
+	if (COMMON_TYPE_LINKS[listType]) {
+		listType = `[${listType}](${COMMON_TYPE_LINKS[listType]})`;
+	}
+
+	return `[List](${KINNAY_WIKI_BASE + '/NEX-Common-Types#list'})<${listType}>`;
+}
+
 module.exports = {
 	generateDocumentation
 };
 
 function isUnicodeSupported() {
 	if (process.platform !== 'win32') {
-		return process.env.TERM !== 'linux'; // Linux console (kernel)
+		return process.env.TERM !== 'linux'; // * Linux console (kernel)
 	}
 
 	return Boolean(process.env.CI)
-		|| Boolean(process.env.WT_SESSION) // Windows Terminal
-		|| process.env.ConEmuTask === '{cmd::Cmder}' // ConEmu and cmder
+		|| Boolean(process.env.WT_SESSION) // * Windows Terminal
+		|| process.env.ConEmuTask === '{cmd::Cmder}' // * ConEmu and cmder
 		|| process.env.TERM_PROGRAM === 'vscode'
 		|| process.env.TERM === 'xterm-256color'
 		|| process.env.TERM === 'alacritty'
